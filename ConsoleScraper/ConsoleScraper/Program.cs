f257e0f477258845ffbc265fd1bf3fce9925612e
Matt Stannett <matt.stannett@gmail.com>
2016-01-03 22:15:05 +1300

    Added XML comments for method intent and TODOs
    This will help with others understand the code and what needs to be done
    to make it better.

--- ConsoleScraper/ConsoleScraper/Program.cs
+++ ConsoleScraper/ConsoleScraper/Program.cs
@@ -19,6 +19,7 @@ namespace ConsoleScraper
 		TODO: Name stuff better
 		TODO: Think about vehicle scoping
 		TODO: static vs const
+		TODO: Make config parameters for constants eg path, whether to make local files etc
 	*/
 
 	class Program
@@ -178,6 +179,13 @@ namespace ConsoleScraper
 			}
 		}
 
+		// TODO: Make parameters into true globals if possible
+		/// <summary>
+		/// This is called inside the worker tasks to crawl the pages asynchronously
+		/// </summary>
+		/// <param name="vehiclePageLinks">A dictionary that contains a indexer for the key, and a link to the wiki page for that vehicle as the value</param>
+		/// <param name="vehicleWikiPageDocuments">An empty dictionary (used as a global) that will be populated with the vehicle name as the key and the HTML content of the page as the value</param>
+		/// <param name="localFileChanges">An empty dictionary (used as a global) that is used to keep track of changes made to local files, vehicle name as the key, and the action performed as the value</param>
 		public static void GetPageHtml(ConcurrentDictionary<int, HtmlNode> vehiclePageLinks, ConcurrentDictionary<string, HtmlDocument> vehicleWikiPageDocuments, ConcurrentDictionary<string, string> localFileChanges)
 		{
 			foreach (var vehiclePageLink in vehiclePageLinks)
@@ -210,6 +218,15 @@ namespace ConsoleScraper
 			}
 		}
 
+		/// <summary>
+		/// Loops through all of the vehicle wiki links that have been provided, attempts to parse the parts that we are interested in -
+		/// the vehicle details table, creates an object from that, then stores the data locally if a flag is set
+		/// </summary>
+		/// <param name="vehicleDetails">An empty dictionary (global) that is populated with the vehicle name and vehicle object</param>
+		/// <param name="vehicleWikiPages">A dictionary that contains links to all of the wiki pages that need parsing</param>
+		/// <param name="errorList">A list (global) that is used to hold any errors that occur</param>
+		/// <param name="indexPosition">The current index we are up to processing - used for error messages</param>
+		/// <param name="expectedNumberOfLinks">The expected number of links to process</param>
 		private static void ProcessWikiHtmlFiles(Dictionary<string, GroundVehicle> vehicleDetails, ICollection<HtmlDocument> vehicleWikiPages, List<string> errorList, GroundVehicleTypeHelper vehicleTypeHelper, VehicleWeightUnitHelper vehicleWeightUnitHelper ,VehicleSpeedUnitHelper vehicleSpeedUnitHelper, VehicleEnginePowerUnitHelper vehicleEnginePowerUnitHelper, VehicleCountryHelper vehicleCountryHelper, VehicleCostUnitHelper vehicleCostUnitHelper, int indexPosition, int expectedNumberOfLinks)
 		{
 			foreach (HtmlDocument vehicleWikiPage in vehicleWikiPages)
@@ -386,6 +403,13 @@ namespace ConsoleScraper
 			}
 		}
 
+		// TODO: Make this handle creating local JSON files as well
+		/// <summary>
+		/// Adds/updates files in the LocalWiki folder
+		/// </summary>
+		/// <param name="localFileChanges">This is being changed to be a global so it will no longer need to be a parameter</param>
+		/// <param name="vehicleWikiPage">The HTML content of the wiki page</param>
+		/// <param name="vehicleName">The vehicle name of the current wiki page</param>
 		private static void UpdateLocalStorageForOfflineUse(ConcurrentDictionary<string, string> localFileChanges, HtmlDocument vehicleWikiPage, string vehicleName)
 		{
 			// Make path to save the local copy of the wiki in so we can run it offline if needs be
