827747b55db531d92a7bf0cc9b29e126f091ae9b
Matt Stannett <matt.stannett@gmail.com>
2016-04-21 21:56:23 +1200

    Removing unused includes, whitespace changes

--- ConsoleScraper/ConsoleScraper/ILogger.cs
+++ ConsoleScraper/ConsoleScraper/ILogger.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleScraper
+﻿namespace ConsoleScraper
 {
 	public interface ILogger
-	{
-
+	{		
 	}
 
 	public class Logger : ILogger
 	{
-
-
 	}
 }
